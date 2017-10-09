2c87df3678c024f2b4b6b1b9c941ebbd8124e36e
2Deo <ziemowit.holda@gmail.com>
2017-10-09 18:00:29 +0200

    Update Devise Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
-Rails.application.routes.draw do
-  get 'pages/home'
+# Rails.application.routes.draw do
+#   get 'pages/home'
+#
+#   root 'pages#home'
+#
+#   devise_for :users
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   root :to => "home#index"
+# end
 
-  root 'pages#home'
+Rails.application.routes.draw do
+  root to: 'pages#home'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => "home#index"
 end
